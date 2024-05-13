6d23a671f83d40b1a67b5fb08f3a8bce83f890a5
ti014 <pnmphuong2001@gmail.com>
2024-05-13 17:28:47 +0700

    update

--- Day1.py
+++ Day1.py
@@ -1,22 +1,36 @@
 '''
-Ok, hôm nay là ngày đầu tiên.
+Ok, hôm nay là ngày code đầu tiên trong chuỗi 100 ngày.
 Với ý tưởng là giả sử bạn của bạn đang thành lập một ban nhạc mới. 
 Nó có thể là rock, idea, pop,... và họ đang gặp khó khăn trong việc tìm một cái tên hay.
-Vậy nên bạn sẽ giúp họ, bằng cách gửi 1 chương trình "tạo tên cho ban nhạc" okerr.
+Vậy nên bạn sẽ giúp họ, bằng cách gửi 1 chương trình "tạo tên ban nhạc".
 
-Rồi, chương trình sẽ hỏi tên thành phố bạn lớn lên.
--> Tôi lớn lên ở Bình Dương.
+Rồi, chương trình sẽ hỏi tên biệt danh của bạn là gì?.
+-> Tôi tên "Ti".
 Tiếp đến chương trình lại hỏi: Thú cưng của tôi là gì?
--> Tôi khá là thích mèo nên chắc chắn phải là mèo rồi.
+-> Tôi khá là thích mèo nên chắc chắn phải là "meo" rồi.
 ----------------------------------------------------------------
 
 Rồi đó, tới đây nó sẽ trả ra tên một ban nhạc là: "BinhDuong meo"
 Nghe cũng hay đó chứ. Bắt đầu viết chương trình hén!!!
+
+----------------------------------------------------------------
+À, để làm được bài này thì bạn nhớ học về:
+    1. Cách in dữ liệu
+    2. Cách xem debug (tức là nếu bạn code bị lỗi thì nó sẽ in ra màn hình cái lỗi đó,
+    bạn phải hiểu lỗi đó ra sao thì mới sửa được code cho đúng)
+    3. Biết cách comment code (giống như mình đang làm cái này cho bạn đọc nè)
+    4. Biết "chuỗi dữ liệu (string)", "biến (variables)". Và cách thao tác với chúng.
+----------------------------------------------------------------
 '''
-# Dòng này test nên bạn có thể xóa dấu '#' để kiểm tra và suy ngẫm một chút nhen
-# print('Wow, hóa ra quê bạn ở ' + input('Thành phố bạn lớn lên tên gì? ') + " ,tuyệt đó!!!")
+
+
+print('Wow, hóa ra quê bạn ở ' + input('Thành phố bạn lớn lên tên gì? ') + " ,tuyệt đó!!!")
 
 print('Chào mừng đến với chương trình đặt tên ban nhạc!')
-thanhPho = input('Thành phố bạn lớn lên là gì?: ')
+thanhPho = input('Biệt danh bạn là gì?: ')
 thuCung = input('Thú cưng của bạn là gì?: ')
 print('Tên ban nhạc của bạn là:', thanhPho + " " + thuCung)
+
+'''
+Bạn thử đổi vị trí của các biến để có thêm nhiều cái tên ban nhạc khác nhau nhen.
+'''
