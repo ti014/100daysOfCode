b4077707c8d4b4ed0725882300ca2ca8258b8a77
ti014 <pnmphuong2001@gmail.com>
2024-05-13 17:08:20 +0700

    update

---
+++ Day1.py
@@ -0,0 +1,22 @@
+'''
+Ok, hôm nay là ngày đầu tiên.
+Với ý tưởng là giả sử bạn của bạn đang thành lập một ban nhạc mới. 
+Nó có thể là rock, idea, pop,... và họ đang gặp khó khăn trong việc tìm một cái tên hay.
+Vậy nên bạn sẽ giúp họ, bằng cách gửi 1 chương trình "tạo tên cho ban nhạc" okerr.
+
+Rồi, chương trình sẽ hỏi tên thành phố bạn lớn lên.
+-> Tôi lớn lên ở Bình Dương.
+Tiếp đến chương trình lại hỏi: Thú cưng của tôi là gì?
+-> Tôi khá là thích mèo nên chắc chắn phải là mèo rồi.
+----------------------------------------------------------------
+
+Rồi đó, tới đây nó sẽ trả ra tên một ban nhạc là: "BinhDuong meo"
+Nghe cũng hay đó chứ. Bắt đầu viết chương trình hén!!!
+'''
+# Dòng này test nên bạn có thể xóa dấu '#' để kiểm tra và suy ngẫm một chút nhen
+# print('Wow, hóa ra quê bạn ở ' + input('Thành phố bạn lớn lên tên gì? ') + " ,tuyệt đó!!!")
+
+print('Chào mừng đến với chương trình đặt tên ban nhạc!')
+thanhPho = input('Thành phố bạn lớn lên là gì?: ')
+thuCung = input('Thú cưng của bạn là gì?: ')
+print('Tên ban nhạc của bạn là:', thanhPho + " " + thuCung)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 100daysOfCode
+Mục đích Project này được tạo ra là để tạo một thử thách Code trong 100 ngày, ai giỏi thì 1 ngày code 100 dòng hoặc 100 file code luôn.
+
+Sẽ có từng file '.py' theo từng ngày, mình push lên chia sẻ cho các bạn coder Việt Nam mới vừa làm quen python. Có đề và cách giải.
+
+Dành cho người Việt thì ngôn ngữ phải là Vietnamese, phong cách nói của GenZ, và phải dễ hiểu phải không nào?
